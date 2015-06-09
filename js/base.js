81f688d6d8ef437d195b2d42f13778787d8d62c0
tedyhy <916610351@qq.com>
2015-06-09 10:45:19 +0800

    event、widthheight

--- js/base.js
+++ js/base.js
@@ -1,8 +1,23 @@
 var $ = function(id) {
 	return document.getElementById(id);
 };
-var $$ = function(cs) {
-	return document.getElementsByClassName(cs);
+var $$ = function(cls) {
+	return getElementsByClassName(cls);
+};
+
+function getElementsByClassName(cls) {
+	try {
+		return document.getElementsByClassName(cls);
+	} catch (e) {
+		var nodes = document.getElementsByTagName("*"),
+			ret = [],
+			node;
+		for (var i = 0, len = nodes.length; i < len; i++) {
+			node = nodes[i];
+			if (hasClass(node, cls)) ret.push(node);
+		};
+		return ret;
+	}
 };
 var XHR = function() {
 	try {
@@ -23,7 +38,7 @@ var XHR = function() {
 		};
 	};
 };
-
+var r_trim = /^\s*(\S*)\s*$/;
 // js
 var codemirrors = $$('codemirror');
 if (codemirrors.length) {
@@ -32,5 +47,44 @@ if (codemirrors.length) {
 			lineNumbers: true,
 			mode: "javascript"
 		});
+	};
+};
+
+function hasClass(ele, cls) {
+	var _cls = ele.className,
+		reg = new RegExp('\\s*\\b' + cls + '\\b\\s*', 'ig');
+	if (reg.exec(_cls)) return true;
+	return false;
+};
+
+function addClass(ele, cls) {
+	var _cls = ele.className,
+		reg = new RegExp('\\s*\\b' + cls + '\\b\\s*', 'ig');
+	if (!reg.exec(_cls)) {
+		ele.className = (_cls + ' ' + cls).replace(r_trim, '$1');
+	};
+};
+
+function removeClass(ele, cls) {
+	var _cls = ele.className,
+		reg = new RegExp('\\s*\\b' + cls + '\\b\\s*', 'ig');
+	if (_cls && reg.exec(_cls)) {
+		ele.className = _cls.replace(reg, '');
+	};
+};
+
+function addEventListener(ele, type, fn) {
+	if (ele.addEventListener) {
+		ele.addEventListener(type, fn, false);
+	} else if (ele.attachEvent) {
+		ele.attachEvent('on' + type, fn);
+	};
+};
+
+function removeEventListener(ele, type, fn) {
+	if (ele.removeEventListener) {
+		ele.removeEventListener(type, fn, false);
+	} else if (ele.detachEvent) {
+		ele.detachEvent('on' + type, fn);
 	};
 };
